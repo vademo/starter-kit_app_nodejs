42d60190580d765b9a2fb88a85f046f2baf79c51
Frank Desmettre <frank.desmettre@gmail.com>
2018-05-22 13:41:00 +0200

    Add frontend serving

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,29 @@
+import fs from 'fs';
 import express from 'express';
 import {} from 'dotenv/config';
 import example from './example';
 
 const router = express.Router();
 
+let fe;
+
 router.use('/api/examples', example);
+
+router.use(express.static('/frontend/build/'));
+
+router.route(['/', '/*']).all((req, res) => {
+  if (fe) {
+    return res.send(fe);
+  }
+
+  fs.readFile('/frontend/build/index.html', 'utf8', (err, index) => {
+    if (err) {
+      return res.send(err);
+    }
+
+    fe = index;
+    res.send(index);
+  });
+});
 
 export default router;
