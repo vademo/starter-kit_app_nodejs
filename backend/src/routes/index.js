db9bd6995dd8ee05bd4e45b5a46587f28787fe11
Frank Desmettre <frank.desmettre@gmail.com>
2018-10-08 10:22:45 +0200

    Fix starter kit

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,29 +1,18 @@
-import fs from 'fs';
+import path from 'path';
 import express from 'express';
 import {} from 'dotenv/config';
 import example from './example';
 
-const router = express.Router();
+const FRONTEND_BUILD_PATH = path.join(__dirname, '../../../frontend/build');
 
-let fe;
+const router = express.Router();
 
 router.use('/api/examples', example);
 
-router.use(express.static('/frontend/build/'));
+router.use(express.static(FRONTEND_BUILD_PATH));
 
-router.route(['/', '/*']).all((req, res) => {
-  if (fe) {
-    return res.send(fe);
-  }
-
-  fs.readFile('/frontend/build/index.html', 'utf8', (err, index) => {
-    if (err) {
-      return res.send(err);
-    }
-
-    fe = index;
-    res.send(index);
-  });
+router.get('*', (req, res) => {
+  res.sendFile(path.join(FRONTEND_BUILD_PATH, 'index.html'));
 });
 
 export default router;
